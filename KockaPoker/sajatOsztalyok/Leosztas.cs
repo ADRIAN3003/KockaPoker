868130c4e2af6a5bfa3e19111390d8690e36e97e
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-04 13:17:12 +0200

    create Leosztas class

---
+++ KockaPoker/sajatOsztalyok/Leosztas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaPoker.sajatOsztalyok
+{
+    class Leosztas
+    {
+        List<int> kockak = new List<int>();
+
+        public Leosztas()
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < 5; i++)
+            {
+                kockak.Add(rnd.Next(1, 6));
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder tmp = new StringBuilder();
+            foreach (var k in kockak)
+            {
+                tmp.Append($"{k} ");
+            }
+
+            return tmp.ToString();
+        }
+    }
+}
