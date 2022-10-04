4a478b42b314436995af7ea08c233c4d9047a8f7
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-04 14:36:56 +0200

    create KepekBeallitasa

--- KockaPoker/SajatOsztalyok/Jatekos.cs
+++ KockaPoker/SajatOsztalyok/Jatekos.cs
@@ -44,9 +44,27 @@ namespace KockaPoker.SajatOsztalyok
 
         public void KepekBeallitasa()
         {
+            int i = 0;
             foreach (var k in kockaKepek)
             {
-                //k.Image = KepValaszt();
+                switch (leosztas.MilyenErtek(i))
+                {
+                    case 1:
+                        k.Image = Properties.Resources.kek1;
+                        break;
+                    case 2:
+                        k.Image = Properties.Resources.kek2;
+                        break;
+                    case 3:
+                        k.Image = Properties.Resources.kek3;
+                        break;
+                    case 4:
+                        k.Image = Properties.Resources.kek4;
+                        break;
+                    case 5:
+                        k.Image = Properties.Resources.kek5;
+                        break;
+                }
             }
         }
     }
